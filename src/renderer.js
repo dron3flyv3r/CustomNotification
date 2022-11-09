bed60ddd1972797c4bea25d67a487f57790e7661
Kasper Horn Larsen <kasperhorn2003@gmail.com>
2022-11-09 13:39:22 +0100

    init map notify and code cleanup

--- src/renderer.js
+++ src/renderer.js
@@ -2,14 +2,17 @@ const mes = document.getElementById("message");
 const tit = document.getElementById("title");
 const box = document.getElementById("box");
 
-var isNotified = false;
 
+
+var isNotified = false;
 
 window.electron.handelUrl((_event, url) => {
 
     let message = "Null";
     let title = "Notification";
     let type = "Null";
+    let la = 0.00;
+    let lo = 0.00;
     let urls;
 
     url = url.slice(1);
@@ -26,6 +29,12 @@ window.electron.handelUrl((_event, url) => {
         if (url.slice(0, 2) == "t=") {
             type = decodeURIComponent(url.slice(2));
         }
+        if (url.slice(0, 3) == "la=") {
+            la = Number(decodeURIComponent(url.slice(3)));
+        }
+        if (url.slice(0, 3) == "lo=") {
+            lo = Number(decodeURIComponent(url.slice(3)));
+        }
     });
 
     if (message != "Null" && !isNotified) {
@@ -35,7 +44,7 @@ window.electron.handelUrl((_event, url) => {
                 break;
         
             case "map":
-            
+                mapNotify(message, title, la, lo);
                 break;
     
             default:
@@ -52,19 +61,28 @@ function boxNotify(message, title) {
     box.style.right = "75px";
     
     new Audio("../src/assets/notification.mp3").play();
-    box.style.animation = "flyFromLeft 0.5s ease-in-out";
+    box.style.animation = "flyFromLeftBox 0.5s ease-in-out";
 
     setTimeout(() => {
         box.style.opacity = "1";
     }, 500);
 
     setTimeout(() => {
-        box.style.animation = "flyAwayToLeft 0.5s ease-in-out";
+        box.style.animation = "flyAwayToLeftBox 0.5s ease-in-out";
         setTimeout(() => {
             box.style.opacity = "0";
         }, 350);
         isNotified = false;
     }, 4500);
+}
+
+function mapNotify(message, title, la, lo) {
+    isNotified = true
+    if (typeof la != "number" && typeof lo != "number") {
+        la, lo = 0.00;
+    }
+    
+    
 }
 
 
